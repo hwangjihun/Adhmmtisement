2b9ea9700b04d36d21cdbc94747c5a9b168efb74
Jihun <115059697+hwangjihun@users.noreply.github.com>
2023-10-09 15:55:01 +0900

    Update advanceCSV.js

--- advanceCSV.js
+++ advanceCSV.js
@@ -1,4 +1,4 @@
-const advanceCSVResultUrl = 'https://adhmmtisement.herokuapp.com/advance/result';
+const advanceCSVResultUrl = 'https://adhmmmtisement.onrender.com/advance/result';
 
 const advanceCSVResultQuery = {
     optionIds: null,
